17c3ad889c290237f3ab77892d376a4313690d58
David Verdeguer <daverlo@github.com>
2020-12-09 09:46:03 +0100

    Solve day9 part1

---
+++ src/days/day9/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func parseInput(path string) ([]int, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	res := make([]int, 0)
+
+	for {
+		var n int
+		_, err := fmt.Fscanf(f, "%d\n", &n)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return nil, err
+			}
+		}
+		res = append(res, n)
+	}
+
+	return res, nil
+}
+
+type Indices struct {
+	I int
+	J int
+}
+
+func part1(numbers []int) int {
+	preamble := numbers[0]
+	numbers = numbers[1:]
+
+	possibleValues := make(map[int]Indices, preamble*preamble/2)
+
+	for i := 0; i < preamble; i++ {
+		for j := i + 1; j < preamble; j++ {
+			value := numbers[i] + numbers[j]
+			// Only update the indices if this is a better solution
+			// It is better if it has a higher low index
+			if j > possibleValues[value].I {
+				possibleValues[value] = Indices{I: i, J: j}
+			}
+		}
+	}
+
+	for i := preamble; i < len(numbers); i++ {
+		indices, ok := possibleValues[numbers[i]]
+		if !ok {
+			return numbers[i]
+		}
+		if indices.I < i-preamble {
+			return numbers[i]
+		}
+
+		for j := i - preamble + 1; j < i; j++ {
+			value := numbers[j] + numbers[i]
+			// Only update the indices if this is a better solution
+			// It is better if it has a higher low index
+			if j > possibleValues[value].I {
+				// Swap i and j to keep the Indices.I as the lowest index
+				possibleValues[value] = Indices{I: j, J: i}
+			}
+		}
+	}
+	return 0
+}
+
+func part2(numbers []int) int {
+
+	return 0
+}
+
+func main() {
+	args := os.Args[1:]
+	input, err := parseInput(args[0])
+	if err != nil {
+		panic(err)
+	}
+
+	output := part1(input)
+	fmt.Println(output)
+
+	// output = part2(input)
+	// fmt.Println(output)
+}
