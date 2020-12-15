98b51d675b44e6f0d3070a7ab787b68a2195b143
David Verdeguer <daverlo@github.com>
2020-12-15 08:13:43 +0100

    Solve day13 part2

--- src/days/day13/main.go
+++ src/days/day13/main.go
@@ -44,8 +44,71 @@ func part1(arrival int, routes []int) int {
 	}
 }
 
-func part2(numbers []int) int {
-	return 0
+type Route struct {
+	Id     int
+	Offset int
+}
+
+func parseInput2(path string) (int, []Route, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	var time int
+	_, _ = fmt.Fscanf(f, "%d\n", &time)
+
+	var line string
+	_, _ = fmt.Fscanf(f, "%s\n", &line)
+
+	routes := make([]Route, 0)
+	offset := -1
+	for _, r := range strings.Split(line, ",") {
+		offset++
+		if r == "x" {
+			continue
+		}
+		v, _ := strconv.Atoi(r)
+		routes = append(routes, Route{Id: v, Offset: offset})
+	}
+
+	return time, routes, nil
+}
+
+func part2(arrival int, routes []Route) int {
+	t := 1
+	step := 1
+	for _, r := range routes {
+		t, step = f(t, step, r.Id, r.Offset)
+	}
+	return t
+}
+
+func f(t int, step int, id int, offset int) (int, int) {
+	// fmt.Println(t, step, id, offset)
+	for {
+		if (t+offset)%id == 0 {
+			return t, LCM(step, id)
+		}
+
+		t += step
+	}
+}
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int) int {
+	result := a * b / GCD(a, b)
+	return result
 }
 
 func main() {
@@ -58,6 +121,10 @@ func main() {
 	output := part1(time, routes)
 	fmt.Println(output)
 
-	// output = part2(input)
-	// fmt.Println(output)
+	time, routes2, err := parseInput2(args[0])
+	if err != nil {
+		panic(err)
+	}
+	output = part2(time, routes2)
+	fmt.Println(output)
 }
